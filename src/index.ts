6fad97634311853254e387569b6da77b772696d7
techyNonso <williamikeji@gmail.com>
2023-01-29 13:42:02 +0100

    wallet connect with address truncate function added

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,81 @@
-
 import algosdk from "algosdk";
 
-import { MyAlgoSession} from './wallets/myalgo'
-import { Asset } from './asset_client';
+import { MyAlgoSession } from "./wallets/myalgo";
+import { Asset } from "./asset_client";
 
 const myAlgo = new MyAlgoSession();
-const algodClient = new algosdk.Algodv2('', "https://node.testnet.algoexplorerapi.io", '');
-const indexerClient = new algosdk.Indexer('', "https://algoindexer.testnet.algoexplorerapi.io", '');
+const algodClient = new algosdk.Algodv2(
+  "",
+  "https://node.testnet.algoexplorerapi.io",
+  ""
+);
+const indexerClient = new algosdk.Indexer(
+  "",
+  "https://algoindexer.testnet.algoexplorerapi.io",
+  ""
+);
 
 async function signer(txns: algosdk.Transaction[]) {
-  const sTxns = await myAlgo.signTxns(txns)
-  return sTxns.map(s => s.blob)
+  const sTxns = await myAlgo.signTxns(txns);
+  return sTxns.map((s) => s.blob);
 }
 
 let APPID = 156293058;
 let ASSETID = 156293328;
 
+//truncate wallet
+const truncate = (
+  text: string,
+  startChars: number,
+  endChars: number,
+  maxLength: number
+): void => {
+  if (text.length > maxLength) {
+    var start = text.substring(0, startChars);
+    var end = text.substring(text.length - endChars, text.length);
+    while (start.length + end.length < maxLength) {
+      start = start + ".";
+    }
+    document.getElementById("connect").innerHTML = `${start + end}`;
+  } else {
+    document.getElementById("connect").innerHTML = `${text}`;
+  }
+};
 
+const buttonIds = [
+  "connect",
+  "create_app",
+  "opt_in",
+  "create_asset",
+  "optin_to_contract",
+  "optin_asset",
+  "get_asset_id",
+  "get_asset_bal",
+  "transfer_to_creator",
+  "get_asset_from_faucet",
+];
+const buttons: { [key: string]: HTMLButtonElement } = {};
+const accountsMenu = document.getElementById("accounts") as HTMLSelectElement;
 
-const buttonIds = ['connect', 'create_app', 'opt_in', 'create_asset', 'optin_to_contract', 'optin_asset', 'get_asset_id', 'get_asset_bal', 'transfer_to_creator', 'get_asset_from_faucet'];
-const buttons: {[key: string]: HTMLButtonElement} = {};
-const accountsMenu = document.getElementById('accounts') as HTMLSelectElement;
+let amountInput = document.getElementById(
+  "transfer_amount"
+) as HTMLInputElement;
 
-let amountInput = document.getElementById("transfer_amount") as HTMLInputElement
-
-
-buttonIds.forEach(id => {
-  buttons[id] = document.getElementById(id) as HTMLButtonElement
-})
+buttonIds.forEach((id) => {
+  buttons[id] = document.getElementById(id) as HTMLButtonElement;
+});
 
 buttons.connect.onclick = async () => {
-  await myAlgo.getAccounts()
-  myAlgo.accounts.forEach(account => {
-    accountsMenu.add(new Option(`${account.name} - ${account.address}`, account.address))
+  await myAlgo.getAccounts();
+  myAlgo.accounts.forEach((account) => {
+    //call function to truncate address
+    truncate(account.address, 4, 4, 11);
+    accountsMenu.add(
+      new Option(`${account.name} - ${account.address}`, account.address)
+    );
     console.log(account);
-    
-  })
-  
-}
-
-
+  });
+};
 
 buttons.create_app.onclick = async () => {
   const AssetApp = new Asset({
@@ -50,48 +86,46 @@ buttons.create_app.onclick = async () => {
 
   const { appId, appAddress, txId } = await AssetApp.create();
 
-  document.getElementById('create_app_status').innerHTML = `App created with id: ${appId} and address: ${appAddress} in txId: ${txId}`;
-  
-}
+  document.getElementById(
+    "create_app_status"
+  ).innerHTML = `App created with id: ${appId} and address: ${appAddress} in txId: ${txId}`;
+};
 
 buttons.create_asset.onclick = async () => {
   const AssetApp = new Asset({
     client: algodClient,
     signer,
     sender: accountsMenu.selectedOptions[0].value,
-    appId: APPID
+    appId: APPID,
   });
 
-  
   const result = await AssetApp.create_token({
     asset_name: String("Algo Smart Coin"),
     unit_name: String("ASC"),
     total_supply: BigInt(20_000_000_000),
-    decimals: BigInt(0)
-
+    decimals: BigInt(0),
   });
   console.log(result);
-  
-}
+};
 
 buttons.optin_to_contract.onclick = async () => {
   const AssetApp = new Asset({
     client: algodClient,
     signer,
     sender: accountsMenu.selectedOptions[0].value,
-    appId: APPID
+    appId: APPID,
   });
 
   const result = await AssetApp.optIn();
-  console.log(result)
-}
+  console.log(result);
+};
 
 buttons.optin_asset.onclick = async () => {
   const AssetApp = new Asset({
     client: algodClient,
     signer,
     sender: accountsMenu.selectedOptions[0].value,
-    appId: APPID
+    appId: APPID,
   });
 
   const opt_txn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
@@ -100,34 +134,38 @@ buttons.optin_asset.onclick = async () => {
     amount: 0,
     suggestedParams: await algodClient.getTransactionParams().do(),
     assetIndex: ASSETID,
-  })
-  const result = await AssetApp.optin_asset({txn: opt_txn});
-  console.log(result)
-}
+  });
+  const result = await AssetApp.optin_asset({ txn: opt_txn });
+  console.log(result);
+};
 
 buttons.transfer_to_creator.onclick = async () => {
   const AssetApp = new Asset({
     client: algodClient,
     signer,
     sender: accountsMenu.selectedOptions[0].value,
-    appId: APPID
+    appId: APPID,
   });
 
-  const result = await AssetApp.transfer_to_creator({amount: BigInt(amountInput.valueAsNumber)});
-  console.log(result)
-}
+  const result = await AssetApp.transfer_to_creator({
+    amount: BigInt(amountInput.valueAsNumber),
+  });
+  console.log(result);
+};
 
 buttons.get_asset_from_faucet.onclick = async () => {
-
   const AssetApp = new Asset({
     client: algodClient,
     signer,
     sender: accountsMenu.selectedOptions[0].value,
-    appId: APPID
+    appId: APPID,
   });
 
-  const result = await AssetApp.get_asset_from_faucet({receiver: accountsMenu.selectedOptions[0].value, time: BigInt(86400)});
+  const result = await AssetApp.get_asset_from_faucet({
+    receiver: accountsMenu.selectedOptions[0].value,
+    time: BigInt(86400),
+  });
   // 2 minutes
-  console.log(result)
-  console.log(AssetApp.getAccountState())
-}
+  console.log(result);
+  console.log(AssetApp.getAccountState());
+};
